4e9b442345cf923654aefe74261df1a7a7e616b2
Naman Talwar <namantalwar07@gmail.com>
2026-09-25 10:52:57 +0530

    Open the app on the learning tabs; fix crash when Android restores the activity
    - App.tsx: make LearningAssistant the initial route instead of the starter home screen
    - MainActivity: call super.onCreate(null), as react-native-screens requires

--- android/app/src/main/java/ai/runanywhere/starter/MainActivity.kt
+++ android/app/src/main/java/ai/runanywhere/starter/MainActivity.kt
@@ -1,11 +1,18 @@
 package ai.runanywhere.starter
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
+
+  // react-native-screens: don't restore fragments after the activity is recreated,
+  // otherwise ScreenFragment fails to instantiate and the app crashes.
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(null)
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
--- src/App.tsx
+++ src/App.tsx
@@ -58,6 +58,7 @@ const App: React.FC = () => {
         <StatusBar barStyle="light-content" backgroundColor={AppColors.primaryDark} />
         <NavigationContainer>
           <Stack.Navigator
+            initialRouteName="LearningAssistant"
             screenOptions={{
               headerStyle: {
                 backgroundColor: AppColors.primaryDark,
